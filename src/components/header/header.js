bc521dc569e12276b99d0e1bb6628482ab38dfed
KStepanI1 <flygovno@mail.ru>
2021-07-16 23:09:08 +0300

    Minor changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,4 @@
-import "@components/header/__buttons/__buttons.js"
 import "@components/header/header.scss"
 import "@components/header/__main-nav/__main-nav.js"
+import "@components/header/__buttons/__buttons.js"
+
