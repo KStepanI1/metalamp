9b56a271252aaab36aa90e4b3b265d0af98cc8a4
KStepanI1 <flygovno@mail.ru>
2021-07-17 10:05:05 +0300

    Minor changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,3 @@
+import "@components/header/__buttons/__buttons.js"
 import "@components/header/header.scss"
 import "@components/header/__main-nav/__main-nav.js"
-import "@components/header/__buttons/__buttons.js"
-
--- src/components/lading-page-content/lading-page-content.js
+++ src/components/lading-page-content/lading-page-content.js
@@ -1,2 +1,1 @@
 import "./lading-page-content.scss"
-import "@components/room-search/room-search.js"
